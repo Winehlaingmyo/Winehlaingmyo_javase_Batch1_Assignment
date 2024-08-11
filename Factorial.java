ac577fca0157107b0ee9531be2b7e5a2f14686f9
Winehlaingmyo <paingmyohtat@gmail.com>
2024-08-11 17:50:26 +0630

    java

---
+++ Factorial.java
@@ -0,0 +1,15 @@
+public class Factorial {
+
+	public static void main(String[] args) {
+		
+		int n =5;
+		
+
+		for(int i = 1; i<=4;i++) {
+			n =n* i;
+			
+			
+   }
+		System.out.println("Factorial of 5 :"+ n);
+	}
+}
---
+++ Multiplatiom.java
@@ -0,0 +1,19 @@
+public class Multiplatiion {
+
+	public static void main(String[] args) {
+		
+		int m =7;
+		int i=0;
+		
+		 
+		while (i<=9){
+			
+			
+			i++;
+			
+		
+		System.out.println(m+"*"+i+"="+m*i);}
+
+	}
+
+}
