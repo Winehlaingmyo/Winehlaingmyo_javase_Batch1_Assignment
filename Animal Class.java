218cd49f5652ee9b2cd44ce954cbb927493fa695
Winehlaingmyo <paingmyohtat@gmail.com>
2024-08-19 21:05:10 +0630

    AnimalMangment

---
+++ Animal Class.java
@@ -0,0 +1,19 @@
+package animaldata;
+
+public class animal {
+
+	public String name;
+	public int age;
+	public String specis;
+	public static int anicount;
+	public static int count = 0;
+	
+	public  void display() {
+		
+		System.out.println("name = " +name);
+		System.out.println("age = "+age);
+		System.out.println("specis = " +specis);
+		
+	}
+
+}
